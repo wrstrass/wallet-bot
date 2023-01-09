7ed3702e5a5210f052344a3ab39587b60f4da5e6
wrstrass <wrstrass@gmail.com>
2023-01-09 03:05:13 +0300

    Basic SQLAlchemy configuration

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import asyncio
 import telegram
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
 
 load_dotenv()
+
+sql_engine = create_engine(
+    os.getenv("DB")
+    + "+"
+    + os.getenv("DB_DRIVER")
+    + "://"
+    + os.getenv("DB_USER")
+    + ":"
+    + os.getenv("DB_PASSWORD")
+    + "@"
+    + os.getenv("DB_HOST")
+    + ":"
+    + os.getenv("DB_PORT")
+    + "/"
+    + os.getenv("DB_DB"),
+    echo=True,
+    future=True,
+)
+sql_engine.connect()
+print(sql_engine)
 
 DEV_ID = int(os.getenv("DEV_ID"))
 
@@ -11,7 +32,7 @@ DEV_ID = int(os.getenv("DEV_ID"))
 async def main():
     bot = telegram.Bot(os.getenv("TOKEN"))
     async with bot:
-        await bot.send_message(DEV_ID, "Loaded")
+        await bot.send_message(DEV_ID, "Loaded\n" + str(sql_engine))
         print(await bot.get_updates())
 
 
